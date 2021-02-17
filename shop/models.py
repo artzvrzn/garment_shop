37d7e2ef028ea308965bb3c250e62dc537a37a4e
Artemon <art.zvrzn@gmail.com>
2021-02-17 23:51:18 +0300

    Added model of goods and category/subcategory

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,40 @@
+import os
 from django.db import models
 
-# Create your models here.
+
+def upload_path(instance, filename):
+    name, extension = filename.split('.')
+    path_name = os.path.join('images', f'{instance.category}', f'image_{instance.pk}.{extension}')
+    return path_name
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    desc = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    stock = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=False)
+    image = models.ImageField(upload_to=upload_path, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    subcategory = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+
+class SubCategory(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
