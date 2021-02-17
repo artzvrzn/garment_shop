96a8bac16598289d16ea127ec6558c9cf245458e
Artemon <art.zvrzn@gmail.com>
2021-02-18 00:51:42 +0300

    set up admin and created super user

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Item, Category, SubCategory
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'updated', 'stock', 'available',)
+    list_filter = ('category', 'subcategory')
+    list_display_links = ('title',)
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category)
+admin.site.register(SubCategory)
