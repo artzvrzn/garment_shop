e5008cf7419ac0c09562409989e44e702f5ac70e
Artemon <art.zvrzn@gmail.com>
2021-02-22 02:53:10 +0300

    function responsible for definition of image path was refactored

--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,18 @@
 import os
 from django.db import models
+from django.utils.deconstruct import deconstructible
 
 
-def upload_path(instance, filename):
-    name, extension = filename.split('.')
-    path_name = os.path.join('images', f'{instance.category}', f'image_{instance.pk}.{extension}')
-    return path_name
+# deconstructible decorator helps to avoid errors during migrations
+@deconstructible
+class Upload:
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, instance, filename):
+        name, extension = filename.split('.')
+        path_name = os.path.join('images', f'{instance.category}', f'{self.name}_{instance.pk}.{extension}')
+        return path_name
 
 
 class Item(models.Model):
@@ -15,7 +22,7 @@ class Item(models.Model):
     updated = models.DateTimeField(auto_now=True)
     stock = models.PositiveIntegerField(default=0)
     available = models.BooleanField(default=False)
-    image = models.ImageField(upload_to=upload_path, blank=True)
+    image = models.ImageField(upload_to=Upload('image'), blank=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     subcategory = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
 
